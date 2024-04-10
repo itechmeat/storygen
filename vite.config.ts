3e8ff91358c451c98d5aaa7caa32eedfebc5a88e
Sergey Eroshenkov <itechmeat@gmail.com>
2024-04-10 19:04:22 +0200

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,44 @@
 import react from '@vitejs/plugin-react'
-import { URL, fileURLToPath } from 'url'
+// import { URL, fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
 // import mkcert from 'vite-plugin-mkcert'
 // import { VitePWA } from 'vite-plugin-pwa'
-import tsconfigPaths from 'vite-tsconfig-paths'
+// import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths({
-      parseNative: false,
-    }),
+    // tsconfigPaths({
+    //   parseNative: false,
+    // }),
     // mkcert(),
     // VitePWA({
     //   registerType: 'autoUpdate',
     // }),
   ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
-      },
-      {
-        find: '@assets',
-        replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
-      },
-      {
-        find: '@styles',
-        replacement: fileURLToPath(new URL('./src/styles', import.meta.url)),
-      },
-    ],
-  },
+  // resolve: {
+  //   alias: [
+  //     {
+  //       find: '@',
+  //       replacement: fileURLToPath(new URL('./src', import.meta.url)),
+  //     },
+  //     {
+  //       find: '@assets',
+  //       replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+  //     },
+  //     {
+  //       find: '@styles',
+  //       replacement: fileURLToPath(new URL('./src/styles', import.meta.url)),
+  //     },
+  //   ],
+  // },
   // server: {
   //   host: true,
   //   port: 4380,
   // },
-  envDir: 'env',
-  envPrefix: ['VITE_'],
+  // envDir: 'env',
+  // envPrefix: ['VITE_'],
   base: '/storygen/',
 })
