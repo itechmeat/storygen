a631d9deea8d7532476d219920e86c40dd91c9f1
Sergey Eroshenkov <itechmeat@gmail.com>
2024-04-10 19:31:36 +0200

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -34,11 +34,10 @@ export default defineConfig({
   //     },
   //   ],
   // },
-  // server: {
-  //   host: true,
-  //   port: 4380,
-  // },
+  server: {
+    host: true,
+    port: 8976,
+  },
   // envDir: 'env',
   // envPrefix: ['VITE_'],
-  base: '/storygen/',
 })
